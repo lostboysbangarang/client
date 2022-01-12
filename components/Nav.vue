e7d05a33d11da8a2624529ec21c5ae5d016d592b
Hayden Lugo <hayden@lugowebdevelopment.com>
2022-01-12 20:08:00 +0100

    mobile nav on top

--- components/Nav.vue
+++ components/Nav.vue
@@ -195,7 +195,7 @@
                         background-color: $charcoal;
                         top: 0;
                         left: 0;
-                        z-index: 1;
+                        z-index: 30;
                         ul {
                             flex-direction: column;
                             li {
